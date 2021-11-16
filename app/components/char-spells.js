b243336f6eef919bfd8770b13a65d20b64f810af
Ren <59793669+SerendipityJones@users.noreply.github.com>
2021-11-16 02:04:44 -0800

    Custom Heal note; tidy combatant edit screen

--- app/components/char-spells.js
+++ app/components/char-spells.js
@@ -1,4 +1,24 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didReceiveAttrs: function () {
+    console.log(this.spells.Life.Heal.special);
+    console.log(this.skills.find(({ name }) => name === 'Life').rating);
+    var cap = this.skills.find(({ name }) => name === 'Life').rating;
+    var times = ""
+    switch(cap) {
+      case 1:
+        times = "once";
+        break;
+      case 2:
+        times = "twice";
+        break;
+      case 3:
+        times = "thrice";
+        break;
+      default:
+        times = cap + " times"
+    }
+    this.spells.Life.Heal.special = this.spells.Life.Heal.special.replace("one time per Life dot",times);
+  }
 });
