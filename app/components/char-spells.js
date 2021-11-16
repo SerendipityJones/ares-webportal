bf408c4eedbb88b6c3e431445cf328d962d1fe00
Ren <59793669+SerendipityJones@users.noreply.github.com>
2021-11-16 02:13:11 -0800

    Fix Heal note; move event list icon

--- app/components/char-spells.js
+++ app/components/char-spells.js
@@ -2,23 +2,23 @@ import Component from '@ember/component';
 
 export default Component.extend({
   didReceiveAttrs: function () {
-    console.log(this.spells.Life.Heal.special);
-    console.log(this.skills.find(({ name }) => name === 'Life').rating);
-    var cap = this.skills.find(({ name }) => name === 'Life').rating;
-    var times = ""
-    switch(cap) {
-      case 1:
-        times = "once";
-        break;
-      case 2:
-        times = "twice";
-        break;
-      case 3:
-        times = "thrice";
-        break;
-      default:
-        times = cap + " times"
+    if (this.spells.Life && this.spells.Life.Heal && this.spells.Life.Heal.special) {
+      var cap = this.skills.find(({ name }) => name === 'Life').rating;
+      var times = ""
+      switch(cap) {
+        case 1:
+          times = "once";
+          break;
+        case 2:
+          times = "twice";
+          break;
+        case 3:
+          times = "thrice";
+          break;
+        default:
+          times = cap + " times"
+      }
+      this.spells.Life.Heal.special = this.spells.Life.Heal.special.replace("one time per Life dot",times);
     }
-    this.spells.Life.Heal.special = this.spells.Life.Heal.special.replace("one time per Life dot",times);
   }
 });
