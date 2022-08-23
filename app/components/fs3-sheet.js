1dddd608716cddf89af9186931b490ce158d1273
Ren <59793669+SerendipityJones@users.noreply.github.com>
2022-08-23 00:19:23 -0700

    Revert "Portal luck conversion"
    This reverts commit 0c503b13ccd0153b7883513fdfa2064487067c7f.

--- app/components/fs3-sheet.js
+++ app/components/fs3-sheet.js
@@ -1,27 +1,4 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  gameApi: service(),
-  flashMessages: service(),
-  
-  actions: {
-    luckTraded() {
-        this.reloadChar();
-    },
-    tradeLuck: function() {
-      let api = this.gameApi;
-      api.requestOne('luckConversion', {
-         id: this.get('char.id')
-       }, null)
-      .then( (response) => {
-        if (response.error) {
-          return;
-        } else if (response.success) {
-          this.flashMessages.success(response.success);
-          this.luckTraded();
-        }
-      });
-    }
-  }
 });
